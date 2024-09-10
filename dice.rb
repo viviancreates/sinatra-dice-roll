7c0eeef8c511aa90ce36cd281b67bdc4625a281d
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-10 01:01:34 +0000

    Add dice example 1

--- dice.rb
+++ dice.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "sinatra/reloader"
 
 get("/") do
   "Hello World"
@@ -14,4 +15,15 @@ end
 
 get("/giraffe") do
   "Hopefully this shows up without having to restart the server 🤞🏾"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
 end
