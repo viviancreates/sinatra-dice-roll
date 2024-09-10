11ecc1e93311c5c7576de47700c79604f2a264b2
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-10 01:12:12 +0000

    Add examples 3 and 4

--- dice.rb
+++ dice.rb
@@ -39,3 +39,29 @@ get("/dice/2/10")  do
   "<h1>2d10</h1>
   <p>#{outcome}</p>"
 end
+
+# (simulate one 20-sided die)
+get("/dice/1/20") do
+  first_die = rand(1..20)
+
+  outcome = "You rolled a #{first_die}."
+
+  "<h1>1d20</h1>
+  <p>#{outcome}</p>"
+end
+
+# (simulate five 4-sided dice)
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4) 
+  fifth_die = rand(1..4) 
+
+  sum = first_die + second_die + third_die + fourth_die + fifth_die
+
+  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die} and a #{fifth_die} for a total of #{sum}."
+
+  "<h1>5d4</h1>
+  <p>#{outcome}</p>"
+end
