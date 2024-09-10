137545cd658e1790a26779d459ee44bd47d92a47
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-10 01:05:16 +0000

    Add dice example 2

--- dice.rb
+++ dice.rb
@@ -27,3 +27,15 @@ get("/dice/2/6") do
   "<h1>2d6</h1>
    <p>#{outcome}</p>"
 end
+
+# (simulate two 10-sided dice)
+get("/dice/2/10")  do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  "<h1>2d10</h1>
+  <p>#{outcome}</p>"
+end
