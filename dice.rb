440caf04e9a2b003c998f89362cf99be699cbb7a
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-10 02:03:37 +0000

    Make changes to the erb page

--- dice.rb
+++ dice.rb
@@ -10,13 +10,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "<h1>Hello World</h1>
-  <ul>
-     <li><a href='/dice/2/6'>Roll two 6-sided dice (2d6)</a></li>
-     <li><a href='/dice/2/10'>Roll two 10-sided dice (2d10)</a></li>
-     <li><a href='/dice/1/20'>Roll one 20-sided die (1d20)</a></li>
-     <li><a href='/dice/5/4'>Roll five 4-sided dice (5d4)</a></li>
-   </ul>"
+  erb(:elephant)
 end
 
 get("/zebra") do
